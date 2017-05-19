51daf55565034b8cb6aa3ca2aa0a827e31751041
mrkkrp <markkarpov92@gmail.com>
2017-05-19 13:58:12 +0700

    Version bump (0.1.3)

--- kill-or-bury-alive.el
+++ kill-or-bury-alive.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Mark Karpov <markkarpov92@gmail.com>
 ;; URL: https://github.com/mrkkrp/kill-or-bury-alive
-;; Version: 0.1.2
+;; Version: 0.1.3
 ;; Package-Requires: ((emacs "24.4") (cl-lib "0.5"))
 ;; Keywords: buffer, killing, convenience
 ;;
